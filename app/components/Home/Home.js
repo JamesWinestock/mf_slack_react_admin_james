36f4221f20bf1143e42d2588094126b88a950fcb
Justin Davidson <jutstindavidson23@gmail.com>
2016-08-16 12:31:41 +1000

    Basic Component setup completed

---
+++ app/components/Home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <h1>Hey from Home Comp</h1>
+  )
+}
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const MainContainer = React.createClass({
   render () {
     return (
-      <h1>Heyyy from Boilerplate!!</h1>
+      <div>
+        {this.props.children}
+      </div>
     )
   }
 })
